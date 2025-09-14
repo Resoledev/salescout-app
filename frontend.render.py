990a46b31b2dfcc6fdfe5bd674987dac4e5da87a
Resoledev <123882861+Resoledev@users.noreply.github.com>
2025-09-14 17:43:39 +0100

    Update frontend.render.py

--- frontend.render.py
+++ frontend.render.py
@@ -463,4 +463,5 @@ def index():
 
 if __name__ == '__main__':
     app.static_folder = 'static'
-    app.run(debug=True, port=5001)
+    # For local testing only
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
